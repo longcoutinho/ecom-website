6568395ae4dffb2af04e398ff69ef57881ddb765
longcoutinho@gmail.com <longdeptraiii2k>
2023-03-31 05:26:30 +0700

    them Menu cho Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,48 @@
 import { Box } from "@mui/material"
 import Container from "@mui/system/Container"
 import Image from "./Image"
+
 export default function Header() {
+    //data
+    const listMenuItem = [
+        {
+            title: 'Trang tin tức huyền học',
+            redirect_link: '#'
+        },
+        {
+            title: 'Sản phẩm phong thủy',
+            redirect_link: '#'
+        },
+        {
+            title: 'Lập lá số phong thủy',
+            redirect_link: '#'
+        },
+        {
+            title: 'Đặt lịch tư vấn',
+            redirect_link: '#'
+        }
+    ]
+    //components
+    const Menu = () => {
+        const listMenu = listMenuItem.map((menuItem) => 
+            <Box className='menu-item' sx={{
+                width: 'auto',
+                height: '100%',
+            }}>
+                <a href={menuItem.redirect_link}>{menuItem.title}</a>
+            </Box>);
+        return (
+            <Box className='menu' sx={{
+                width: '100%',
+                height: '15%',
+                display: 'flex',
+                flexDirection: 'row'
+            }}>
+                {listMenu}
+            </Box> 
+        )
+    }
+    //export components
     return (
         <Container disableGutters maxWidth={false} sx={{
             height: 200,
@@ -16,7 +57,7 @@ export default function Header() {
             }}/> 
             <Box className='logo and banner' sx={{
                 width: '100%',
-                height: '80%',
+                height: '75%',
                 display: 'flex',
                 flexDirection: 'row'
             }}> 
@@ -41,11 +82,7 @@ export default function Header() {
                     }} />   
                 </Box>          
             </Box>     
-            <Box className='line' sx={{
-                width: '100%',
-                height: '10%',
-                backgroundColor: '#8F0101'
-            }}/> 
+            <Menu></Menu>
         </Container>
     )
 }
