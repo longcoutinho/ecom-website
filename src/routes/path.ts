b7fb56d84aefee5a9d49b66ac3d0c612d3f42865
longcoutinho@gmail.com <longdeptraiii2k>
2023-04-26 07:20:53 +0700

    abc

--- src/routes/path.ts
+++ src/routes/path.ts
@@ -9,16 +9,13 @@ export const PATH_PAGE = {
   login: path(ROOTS_PAGE, "/login"),
   user: {
     tab1: ROOTS_PAGE,
-    tab2: path(ROOTS_PAGE, "sanpham"),
-    tab3: path(ROOTS_PAGE, "lapla"),
-    tab4: path(ROOTS_PAGE, "datlich"),
+    tab2: path(ROOTS_PAGE, "posts"),
+    tab3: path(ROOTS_PAGE, "items"),
+    tab4: path(ROOTS_PAGE, "lapla"),
   },
   lapla: {
     root: path(ROOTS_PAGE, "lapla"),
     tuvi: path(ROOTS_PAGE, "lapla/tuvi"),
     luchao: path(ROOTS_PAGE, "lapla/luchao"),
-  },
-  admin: {
-    newPost: path(ROOTS_PAGE, "admin/create-post"),
   },
 };
